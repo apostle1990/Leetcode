a75b2f4ea7186c7dacfef2ebb48ba9e8f763ba90
apostle1990 <823353049@qq.com>
2021-04-06 15:44:53 +0800

    prioriqueue

---
+++ src/chapter9/KthLargest.java
@@ -0,0 +1,33 @@
+package chapter9;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+//703数据流中的第 K 大元素
+public class KthLargest {
+    private PriorityQueue<Integer> minheap;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        minheap = new PriorityQueue<>();
+        this.k = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minheap.add(val);
+        if (minheap.size()>k) minheap.poll();
+        return minheap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums ={};
+        int k =1;
+        KthLargest kthLargest = new KthLargest(k,nums);
+        int re = kthLargest.add(-3);
+        System.out.println(re);
+    }
+}
